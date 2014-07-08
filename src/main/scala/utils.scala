4c8aeb7e3869d4f4ea2c3dd201fdaf7b3036b649
Wes Freeman <wes@skeweredrook.com>
2014-07-07 23:20:11 -0400

    fixing exit so it cleans up term...

--- src/main/scala/utils.scala
+++ src/main/scala/utils.scala
@@ -25,37 +25,44 @@ package object Utils {
   def logFirstPass(processStartTime: Long, lines: Long) = {
     val curTime = System.currentTimeMillis
     if (lines % 100000 == 0 && lines != 0) {
-      logStatus(processStartTime, lines)
-      terminal.moveCursor(10, 5)
-      putString("first pass (collecting machine ids)...")
-      terminal.moveCursor(10, 6)
       var elapsed = curTime - processStartTime
       if (elapsed == 0) elapsed = 1000
-      putString("%s elapsed".format(formatTime(elapsed)))
-      terminal.moveCursor(10, 7)
       val thousands:Long = lines / 1000
       val millions:Long = lines / 1000000
       val avgKRate:Double = thousands / (elapsed / 1000)
       val total = 2630000000L
-      putString("%dM lines read".format(millions))
+      logStatus(processStartTime, lines)
+      terminal.moveCursor(10, 4)
+      putString("first pass (collecting machine ids)...")
+      terminal.moveCursor(10, 5)
+      putString("%s elapsed  ".format(formatTime(elapsed)))
+      terminal.moveCursor(10, 6)
+      putString("%dM lines read  ".format(millions))
+      terminal.moveCursor(10, 7)
+      putString("%.0fK lines/sec (avg.)     ".format(avgKRate))
       terminal.moveCursor(10, 8)
-      putString("%.0fK lines/sec (avg.)  ".format(avgKRate))
+      putString("%2.2f%% complete (approx.)    ".format(lines.toDouble / total * 100))
       terminal.moveCursor(10, 9)
-      putString("%2.2f%% complete (approx.) ".format(lines.toDouble / total * 100))
-      terminal.moveCursor(10, 10)
       putString("%s time remaining (approx.)                   ".format(formatTime(((total - lines) / avgKRate).toLong)))
     }
   }
 
   def logStatus(processStartTime: Long, rdfLineCount: Long) = {
     val curTime = System.currentTimeMillis
-    val key = terminal.readInput()
-    if (key != null && key.isCtrlPressed && key.getCharacter == 'c') System.exit(0)
+    checkForExit
     if (rdfLineCount % (ONE_MILLION * 10L) == 0) {
       logger.info(": " + rdfLineCount / 1000000 + "M tripleString lines processed" +
         "; last 10M: " + formatTime(curTime - lastTime) +
         "; process elapsed: " + formatTime(curTime - processStartTime))
       lastTime = curTime
+    }
+  }
+
+  def checkForExit = {
+    val key = terminal.readInput()
+    if (key != null && key.isCtrlPressed && key.getCharacter == 'c') {
+      cleanupTerminal
+      System.exit(0)
     }
   }
 
